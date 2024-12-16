55233178e7f2703f60c9214f996985c9f0c0d3f4
Albin <alvinfer67@gmail.com>
2024-12-16 17:48:48 -0600

    registring actions for remove in current file and in project

---
+++ src/actions.ts
@@ -0,0 +1,14 @@
+import * as vscode from "vscode";
+
+
+/**
+ * Remove unused imports in the current file.
+ */
+export async function removeUnusedImportsInCurrentFile(): Promise<void> {
+  
+}
+
+export async function removeUnusedImportsInProject(): Promise<void> {
+
+}
+
