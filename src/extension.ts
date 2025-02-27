2ffdfba9e7db108cb878974a8e239f69c1e12482
Albin <alvinfer67@gmail.com>
2025-02-26 18:22:55 -0600

    refactor: file name changed before(actions) now (extensionActions)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-import { removeUnusedImportsInCurrentFile, removeUnusedImportsInProject } from './actions';
+import { removeUnusedImportsInCurrentFile, removeUnusedImportsInProject } from './extensionActions';
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
