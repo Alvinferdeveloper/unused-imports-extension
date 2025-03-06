4efa0e0b236c83fd9e731d69a04a43130c378233
Albin <alvinfer67@gmail.com>
2025-03-06 13:58:11 -0600

    test: default import being used

--- src/test/usedImportCases.test.ts
+++ src/test/usedImportCases.test.ts
@@ -2,25 +2,48 @@ import { removeUnusedImports } from '../imports';
 import * as assert from 'assert';
 
 suite('Extension Test Suite for used imports', () => {
-    test('keep usedImports - one used import on a single line', () => {
-        const inputText = `
+
+  test('keep usedImports - default import being used', () => {
+    const inputText = `
+      import defaultModule from 'module';
+      const result = defaultModule.something();
+    `;
+    const result = removeUnusedImports(inputText);
+    assert.strictEqual(result.newLines.trim(), inputText.trim());
+    assert.strictEqual(result.unusedImportsPresents, false);
+  });
+
+  test('keep usedImports - one used import on a single line', () => {
+    const inputText = `
       import { something } from 'module';
       const x = 10;
       const result = something();
     `;
-        const result = removeUnusedImports(inputText);
-        assert.strictEqual(result.newLines.trim(), inputText.trim());
-        assert.strictEqual(result.unusedImportsPresents, false);
-    });
+    const result = removeUnusedImports(inputText);
+    assert.strictEqual(result.newLines.trim(), inputText.trim());
+    assert.strictEqual(result.unusedImportsPresents, false);
+  });
 
-    test('keep usedImports - one used import on a single line with type import', () => {
-        const inputText = `
+  test('keep usedImports - multiple used named imports on a single line', () => {
+    const inputText = `
+    import { something, somethingElse } from 'module';
+    const x = 10;
+    const result = something();
+    const result2 = somethingElse();
+  `;
+    const result = removeUnusedImports(inputText);
+    assert.strictEqual(result.newLines.trim(), inputText.trim());
+    assert.strictEqual(result.unusedImportsPresents, false);
+  });
+
+  test('keep usedImports - one used import on a single line with type import', () => {
+    const inputText = `
           import type { MyType } from 'module';
           import { usedFunction } from 'module';
           const x: MyType = usedFunction();
         `;
-        const result = removeUnusedImports(inputText);
-        assert.strictEqual(result.newLines.trim(), inputText.trim());
-        assert.strictEqual(result.unusedImportsPresents, false);
-      });
+    const result = removeUnusedImports(inputText);
+    assert.strictEqual(result.newLines.trim(), inputText.trim());
+    assert.strictEqual(result.unusedImportsPresents, false);
+  });
 });
