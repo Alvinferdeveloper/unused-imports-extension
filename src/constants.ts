c4187572ca6b9e60447669fe9ec350adb7801e59
Albin <alvinfer67@gmail.com>
2025-03-08 23:15:30 -0600

    update: regex for wild card import to match type wild card imports

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const importRegexs = {
     DEFAULT_IMPORT: /^import\s+([a-zA-Z_$][a-zA-Z_$0-9]*)\s+from/,
     NAMED_IMPORT: /import\s+\{\s*([^}]+)\s*\}\s+from/,
-    WILD_CARD_IMPORT: /^import\s+\*\s+as\s+([a-zA-Z_$][a-zA-Z_$0-9]*)\s+from/,
+    WILD_CARD_IMPORT: /^import\s+(?:type\s+)?\*\s+as\s+([a-zA-Z_$][a-zA-Z_$0-9]*)\s+from/,
     TYPE_IMPORT: /^import\s+type\s+\{\s*([^}]+)\s*\}\s+from/,
     COMBINED_IMPORT: /^import\s+([a-zA-Z_$][a-zA-Z_$0-9]*)?,?\s*\{\s*([^}]+)\s*\}\s+from/,
     VALID_IDENTIFIER: /(?:(["'`])(?:\\.|(?!\1)[^\\])*\1)|\b[a-zA-Z_$][a-zA-Z_$0-9]*(?!\s*:)/g,
